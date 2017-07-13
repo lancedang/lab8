68090df26a736176abb7fb0b97f9527c15d8e3db
qiankai02 <qiankai02@ppdai.com>
2017-07-13 12:51:29 +0800

    add drivertest

---
+++ src/test/com/ppdai/tutorial/DriverTest.java
@@ -0,0 +1,35 @@
+package test.com.ppdai.tutorial;
+
+import com.ppdai.tutorial.Driver;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Driver Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>ÆßÔÂ 13, 2017</pre>
+ */
+public class DriverTest {
+    private Driver driver;
+    @Before
+    public void before() throws Exception {
+        driver = new Driver();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: main(String[] args)
+     */
+    @Test
+    public void testMain() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+} 
